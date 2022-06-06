151f02e1d2fa4d2b5833e276373d8f7b76b75bbd
Lucas Souza <lucasamarosouza1@gmail.com>
2022-06-06 16:46:32 -0300

    atividade finalizada

--- Lógica de Programação/operadores, funções e estruturas de decisão e repetição/lista_de_exercicios_01.js
+++ Lógica de Programação/operadores, funções e estruturas de decisão e repetição/lista_de_exercicios_01.js
@@ -1,5 +1,4 @@
-/*Como está a aprendizagem dos conceitos de lógica de programação?  Vamos praticar?
-1) Faça uma função que:
+/*1) Faça uma função que:
 a) Receba código da peça
 b) Receba valor da peça
 c) Receba Quantidade de peças
@@ -18,3 +17,37 @@ function pecas(cod,valor,quant)
 }
 
 pecas(101,10,2)
+
+/*2) Faça uma função para calcular o estoque médio de uma peça,
+sendo que ESTOQUEMÉDIO = (QUANTIDADE MÍNIMA + QUANTIDADE MÁXIMA) /2
+*/
+
+function estoqueMedio (quantmin,quantmax)
+{
+    let media = (quantmin + quantmax)/2
+    console.log(media)
+}
+estoqueMedio(8,5)
+
+/*3) Faça uma função que:
+a) Receba a cotação do dólar
+b) Receba um valor em dólares
+c) Converta esse valor para Real
+d) Mostre o resultado
+*/
+
+function conversaoDolar (cotacao,valorDolar)
+{
+    let conversao = cotacao * valorDolar
+
+    console.log("O valor convertido para real é: " + conversao)
+}
+conversaoDolar (4.80, 5)
+
+/*4) Sabendo que A=3, B=7 e C=4, informe se as expressões abaixo são verdadeiras ou falsas.
+a) (A + C) > B  false         
+b) B >= (A + 2) true       
+c) C = (B –A)   true     
+d) (B + A) <= C false        
+e) (C + A) > B  false
+*/
